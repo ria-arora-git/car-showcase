4ee1dbcaa5f30f8ce7a6ceb7cc469d63740d074e
Ishaan vats <Ishaanvats74@gmail.com>
2025-04-18 19:12:56 +0530

    47:30 completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,8 +20,8 @@ const Hero = () => {
       <div className='xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen'>
         <div className='relative xl:w-full w-[90%] xl:h-full h-[590px] z-0'>
           <Image src={'/hero.png'} className='object-contain' alt='hero'  fill />
-          <div className='absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden'></div>
         </div>
+        <div className='absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden'></div>
       </div>
     </div>
   )
